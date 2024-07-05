a8fa46672965dbb2739fa29a00f11182d6dbafca
SebastianSalasR <sebastian_salas_r@hotmail.com>
2024-07-04 21:51:18 -0400

    Actualizado el css

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -12,9 +12,12 @@ function DashboardPage() {
 	return (
 		<main className="h-[calc(100vh-7rem)] flex flex-col space-y-10 justify-center items-center bg-bonito-1">
 
+
+			<div className="flex flex-row space-x-10">
 				<Registerclient />
 				<RegisterWorker />
 				<RegisterService />
+			</div>
 
 		</main>
 	);
